1b94077a7fa8b48d9f6f34428c2571bab38ad9b7
greenazza <greenazza@gmail.com>
2015-10-07 22:14:03 +1300

    Source code for simple heads and tails game

---
+++ Heads or tails game.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipCoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int coin;
+            string userGuess;
+            Random rng = new Random();
+
+            // Query the user
+            Console.Write("Enter your Guess Heads or Tails ");
+            userGuess = Console.ReadLine();
+
+            //Get a random number for the coin flip
+
+            coin = rng.Next(0, 2);
+
+            if (coin == 0 && userGuess == "T, t, Tails, tails")
+            {
+                Console.WriteLine("The coin flip is tails, you win!");
+            }
+            else if (coin == 1 && userGuess == "H")
+            {
+                Console.WriteLine("The coin flip is Heads, you win");
+            }
+            else
+            {
+                if (coin == 0)
+                {
+                    Console.WriteLine("The coin flip is tails, try again");
+                }
+                else
+                {
+                    Console.WriteLine("The coin flip is heads, try again");
+
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+     
+    }
+}
+    
+
---
+++ New way of doing the heads or tails game.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewWayCoinFlip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int coin;
+            string userGuess;
+            string coinDescription = "T";
+            Random rng = new Random();
+
+            //Query the user for heads or tails
+
+            Console.Write("Enter your guess H or T ");
+            userGuess = Console.ReadLine();
+
+            //get a random number for the coin flip
+
+            coin = rng.Next(0, 2);
+            if (coin == 1)
+            {
+                coinDescription = "H";
+
+            }
+            if ((coin == 0 && userGuess == "T") || (coin == 1 && userGuess == "H"))
+            {
+                Console.WriteLine("The coin flip was {0}, you win", coinDescription);
+            }
+            else
+            {
+                Console.WriteLine("Coin flip is {0}, you lose", coinDescription);
+            }
+
+            Console.ReadLine();
+        }
+        
+    }
+}
